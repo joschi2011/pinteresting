2cdcdd8d33fe7958e05463bf8512f1d47c10d85f
Simone <sjaschko@yahoo.com>
2015-09-10 14:31:56 +0000

    Fixing unassigned pins

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -46,8 +46,8 @@ class PinsController < ApplicationController
     end
 
     def correct_user
-      @pin = current_user.pins.find_by(id: params[:id])
-      redirect_to pins_path, notice: "Not authorized to edit this pin" if @pin.nil?
+     # @pin = current_user.pins.find_by(id: params[:id])
+     # redirect_to pins_path, notice: "Not authorized to edit this pin" if @pin.nil?
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
